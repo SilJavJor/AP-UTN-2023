6c5879c04e98f9ed31784e2dcca294d71108fc49
SilJavJor <silvio.jorda@gmail.com>
2023-04-25 21:34:16 -0300

    Prueba de Actualizacion de del JDK

--- tp-maven/src/main/java/com/mycompany/tp/maven/TpMaven.java
+++ tp-maven/src/main/java/com/mycompany/tp/maven/TpMaven.java
@@ -10,7 +10,7 @@ package com.mycompany.tp.maven;
 public class TpMaven {
 
     public static PronosticoDeportivo PRODE;
-
+    
     public static void main(String[] args) {
         System.out.println ();
         System.out.println ("Sistema de simulación de pronósticos deportivos.");
